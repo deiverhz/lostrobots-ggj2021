49d568c3b873a5b507bae74c7e8b5e79cda67a85
deiverhz <deiverhs@outlook.com>
2025-09-04 18:15:50 -0600

    Add initial server setup with Socket.IO and player management
    - Created package.json to manage dependencies and scripts
    - Implemented server.js to handle player connections, movements, and disconnections
    - Added CORS support for specific origins
    - Established player data structure and communication for position updates

---
+++ server.js
@@ -0,0 +1,70 @@
+var server = require("http").createServer();
+const validOrigins = ["https://launch.playcanvas.com", "https://playcanv.as"];
+
+var countPlayers = 0;
+var maxPlayers = 2;
+
+const io = require("socket.io")(server, {
+  cors: {
+    origin: validOrigins,
+    methods: ["GET", "POST"],
+  },
+});
+
+var players = {};
+
+function Player(id) {
+  this.id = id;
+  this.x = 0;
+  this.y = 0;
+  this.z = 0;
+  this.rx = 0;
+  this.ry = 0;
+  this.rz = 0;
+  this.entity = null;
+}
+
+io.on("connection", (socket) => {
+  socket.on("initialize", function () {
+    countPlayers++;
+    var id = socket.id;
+    var newPlayer = new Player(id);
+    players[id] = newPlayer;
+
+    socket.emit("playerData", {
+      id: id,
+      players: players,
+      countPlayers: countPlayers,
+    });
+    socket.broadcast.emit("playerJoined", {
+      newPlayer: newPlayer,
+      countPlayers: countPlayers,
+    });
+  });
+
+  socket.on("positionUpdate", function (data) {
+    if (!players[data.id]) return;
+    players[data.id].x = data.x;
+    players[data.id].y = data.y;
+    players[data.id].z = data.z;
+    players[data.id].rx = data.rx;
+    players[data.id].ry = data.ry;
+    players[data.id].rz = data.rz;
+
+    socket.broadcast.emit("playerMoved", data);
+  });
+
+  socket.on("disconnect", function () {
+    if (!players[socket.id]) return;
+    delete players[socket.id];
+    // Update clients with the new player killed
+    countPlayers--;
+    socket.broadcast.emit("killPlayer", {
+      sid: socket.id,
+      countPlayers: countPlayers,
+    });
+  });
+});
+
+console.log("Server started.");
+server.listen(3000);
